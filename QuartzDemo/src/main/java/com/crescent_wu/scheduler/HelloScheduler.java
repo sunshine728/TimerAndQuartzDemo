3142391d8631c9eb4b7d9330da346ffa160fa370
crescent_wu <812693082@qq.com>
2018-05-02 21:21:51 +0800

    Quartz相关的小demo 讲解了SimpleQuartz的几种用法。

--- QuartzDemo/src/main/java/com/crescent_wu/scheduler/HelloScheduler.java
+++ QuartzDemo/src/main/java/com/crescent_wu/scheduler/HelloScheduler.java
@@ -19,17 +19,36 @@ public class HelloScheduler {
                 .withIdentity("myJob","group1").build();
        //获取距离当前时间3秒后的时间
 //        date.setTime(date.getTime()+3000);
-//        Date endDate = new Date();
-//        endDate.setTime(endDate.getTime()+6000);
 
-        //创建一个Trigger实例，定义该job立即执行并每隔两秒重复执行一次，直到永远
-        Trigger trigger = TriggerBuilder.newTrigger().
-                withIdentity("myTrigger","group1")
+        //获取距离当前时间4秒钟之后的具体时间
+        date.setTime(date.getTime()+4000);
+        //获取距离当前时间6秒钟之后的具体时间
+
+        Date endDate = new Date();
+        endDate.setTime(endDate.getTime()+6000);
+        //距离当前时间4秒钟后执行且仅执行一次任务
+//        SimpleTrigger trigger = (SimpleTrigger)TriggerBuilder.newTrigger().
+//                withIdentity("myTrigger","group1")
 //                .startAt(date)
-//                .endAt(endDate)
+//                .build();
+        //距离当前时间4秒钟后执行首次任务，并之后每隔两秒钟重复执行一次
+
+//        SimpleTrigger trigger = (SimpleTrigger)TriggerBuilder.newTrigger().
+//                withIdentity("myTrigger","group1")
+//                .startAt(date)
+//                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+//                .withIntervalInSeconds(2).withRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY))
+//                .build();
+
+        //距离当前时间4秒钟后执行首次任务，并之后每隔两秒钟重复执行一次
+        //直到距离当前时间6秒钟之后的具体时间
+        SimpleTrigger trigger = (SimpleTrigger)TriggerBuilder.newTrigger().
+                withIdentity("myTrigger","group1")
+                .startAt(date)
+                .endAt(endDate)
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                        .withIntervalInSeconds(2).repeatForever())
- .build();
+                        .withIntervalInSeconds(2).withRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY))
+                .build();
         //创建Scheduler实例
         SchedulerFactory schedulerFactory = new StdSchedulerFactory();
         Scheduler scheduler = schedulerFactory.getScheduler();
